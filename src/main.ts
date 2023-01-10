0e002ec28c9058aae628825f834b505bb3c8a797
alexwallden <alexander.wallden@me.com>
2023-01-10 08:12:38 +0100

    :construction: working on menu animation

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import './style/style.css';
 
 const menuBtn: HTMLButtonElement | null = document.querySelector('#menu-btn');
-const sausageTop: any = document.querySelector('#sausage_x5F_top');
+const sausageTop: SVGGElement | null = document.querySelector('#sausage_x5F_top');
 
 let clicked = false;
 
 menuBtn?.addEventListener('click', () => {
   clicked = !clicked;
-  sausageTop.style.transform = clicked ? 'rotate(18deg)' : 'rotate(0deg)';
+  if (sausageTop) { sausageTop.style.transform = clicked ? 'rotate(18deg)' : 'rotate(0deg)'; }
 })
