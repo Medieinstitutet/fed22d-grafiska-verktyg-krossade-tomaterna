b453f74d14bfbb6eeac857c6721b04e254757827
hannaforssell <forssellhanna@gmail.com>
2023-01-14 19:01:11 +0100

    ✨ feat: adds 2nd booking form
    and appears when clicking on booking button

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,11 @@ const acceptCookiesBtn: HTMLButtonElement | null = document.querySelector('#acce
 const declineCookiesBtn: HTMLButtonElement | null = document.querySelector('#decline-cookies');
 const cookiesApproved: boolean | null = JSON.parse(localStorage.getItem('cookiesApproved') as string) as boolean;
 
+const bookTableBtnTop = document.querySelector('#top-book-table') as HTMLButtonElement;
+const bookingFormTop = document.getElementById('bookingFormTop') as HTMLFormElement;
+const bookTableBtnBottom = document.querySelector('#bottom-book-table') as HTMLButtonElement;
+const bookingFormBottom = document.getElementById('bookingFormBottom') as HTMLFormElement;
+
 let clicked = false;
 
 if (cookiesApproved) {
@@ -53,14 +58,13 @@ menuBtn?.addEventListener('click', toggleNavMenu);
 acceptCookiesBtn?.addEventListener('click', acceptCookies);
 declineCookiesBtn?.addEventListener('click', declineCookies);
 
-// BOOKING FORM POPUP
-
-const bookTableBtn = document.querySelector('#test') as HTMLButtonElement;
-
-const bookingForm = document.getElementById('bookingForm') as HTMLFormElement;
-
-function displayBookingForm() {
-  bookingForm.style.display = 'block';
+function displayTopBookingForm() {
+  bookingFormTop.style.display = 'block';
 }
 
-bookTableBtn.addEventListener('click', displayBookingForm);
+function displayBottomBookingForm() {
+  bookingFormBottom.style.display = 'block';
+}
+
+bookTableBtnTop.addEventListener('click', displayTopBookingForm);
+bookTableBtnBottom.addEventListener('click', displayBottomBookingForm);
